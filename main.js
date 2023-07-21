0055c30126c6a0c49508a609cb20b1fd59bff68a
bilal-dev2006 <elemranibilal2006@gmail.com>
2023-07-21 17:01:17 +0100

    add target blank and add margin bottom to show more btn

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Project : Image Search App
-
 const accessKey = "5oOmM9xjz0irL0PtDdR0tLdxPX3wWhJGpuncS4Yq5G4";
 const form = document.forms[0];
 const submitBtn = document.querySelector("button");
@@ -7,47 +6,41 @@ const input = document.querySelector("input");
 const cardContent = document.querySelector(".cards-content");
 const showMoreBtn = document.querySelector("section button");
 const searchStatus = document.querySelector("h3");
-
 let page = 1;
-
-// search button styling
-input.oninput = function () {
+// Add event listener for input value change
+input.addEventListener("input", function () {
   if (input.value !== "") {
     submitBtn.classList.add("good-btn");
   }
-};
-input.onblur = function () {
+});
+// Add event listener for input blur
+input.addEventListener("blur", function () {
   if (input.value === "") {
     submitBtn.classList.remove("good-btn");
   }
-};
-submitBtn.onclick = function (e) {
+});
+// Add event listener for submit button click
+submitBtn.addEventListener("click", function (e) {
   if (!submitBtn.classList.contains("good-btn")) {
     e.preventDefault();
   }
-};
-//
-
-form.onsubmit = function (e) {
+});
+// Add event listener for form submission
+form.addEventListener("submit", function (e) {
   e.preventDefault();
   showMoreBtn.classList.remove("show");
   submitBtn.classList.remove("good-btn");
   cardContent.textContent = "";
-
   let search = input.value;
-  // make input
   input.value = "";
-
-  // create XML Request
   fetchImg(search);
-
-  showMoreBtn.onclick = function () {
+  // Add event listener for show more button click
+  showMoreBtn.addEventListener("click", function () {
     ++page;
     fetchImg(search);
-  };
-};
-
-// fetch function
+  });
+});
+// Function to fetch images
 function fetchImg(search) {
   const myRequest = new XMLHttpRequest();
   myRequest.open(
@@ -55,7 +48,6 @@ function fetchImg(search) {
     `https://api.unsplash.com/search/photos?page=${page}&query=${search}&client_id=${accessKey}`
   );
   myRequest.send();
-
   myRequest.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       const myData = JSON.parse(this.responseText);
@@ -66,7 +58,6 @@ function fetchImg(search) {
         searchStatus.textContent = "Results For " + search;
         showMoreBtn.classList.add("show");
       }
-
       for (let i = 0; i < myData.results.length; i++) {
         let card = document.createElement("div");
         card.classList.add("card");
@@ -81,18 +72,16 @@ function fetchImg(search) {
         div.appendChild(span);
         card.appendChild(div);
         cardContent.appendChild(card);
-
         let downloadLink = document.createElement("a");
         downloadLink.href = myData.results[i].urls.small;
-        downloadLink.setAttribute("download", "");
+        downloadLink.setAttribute("target", "_blank");
         let downloadImg = document.createElement("img");
         downloadImg.src = "imgs/icons8-download-24.png";
         downloadLink.appendChild(downloadImg);
         div.appendChild(downloadLink);
-
-        card.onclick = function () {
+        card.addEventListener("click", function () {
           card.classList.toggle("showed");
-        };
+        });
       }
     }
   };
